5275e31f682ebb9904d58e7b9d82611f7f1fef02
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-18 22:55:22 +0900

    SpotifyAPIを利用できるように設定

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -5,5 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('index/', include('frontend.urls'))
+    path('index/', include('frontend.urls')),
+    path('', include('frontend.urls')),
+    path('spotify/', include('spotify.urls')),
 ]
