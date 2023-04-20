1c95482ad86730c3bab59a515345ae93b32b9fbd
MahiroWatanabe <konchikuwa2@gmail.com>
2023-04-20 22:00:23 +0900

    infoのURL追加

--- music_controller/frontend/urls.py
+++ music_controller/frontend/urls.py
@@ -4,8 +4,9 @@ from .views import index
 app_name = 'frontend'
 
 urlpatterns = [
-    path('', index,name=''),
+    path('', index, name=''),
     path('join/', index),
+    path('info/', index),
     path('create/', index),
     path('room/<str:roomCode>', index)
 ]
